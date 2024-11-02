45066a764d47a3afd56a7845511a7f7f9cebd434
Yannick Daniel Gibson <yannick.gibson@seznam.cz>
2024-11-02 22:32:53 +0100

    update

--- check.py
+++ check.py
@@ -3,7 +3,7 @@ from Bio.pairwise2 import format_alignment
 
 
 def is_match_pairwise2(gene: str, sequence: str, verbose_if_matched=True, verbose=False) -> tuple[bool, float]:
-    THRESHOLD = 1000
+    SCORE_PERCENT_THRESHOLD = 0.4
 
     # Find alignment
     match_bonus = 2
@@ -12,13 +12,20 @@ def is_match_pairwise2(gene: str, sequence: str, verbose_if_matched=True, verbos
     gap_penalty = -2
     _alignments = pairwise2.align.localms(sequence, gene, match_bonus, mismatch_penalty, gap_penalty, gap_extending_penalty, one_alignment_only=True)
 
+    if len(_alignments) == 0:
+        return False, 0
+
+    score = _alignments[0].score
+    max_score = (len(gene) * 3)
+    score_percent = score / max_score
+    print(score, max_score, score_percent)
+
     # Evaluate result
-    if len(_alignments) > 0 and _alignments[0].score > THRESHOLD:
+    if len(_alignments) > 0 and score > score_percent > SCORE_PERCENT_THRESHOLD:
         if verbose_if_matched:
             print(format_alignment(*_alignments[0]))
-        elif verbose:
-            print(f"Score: {_alignments[0].score}")
-        return True, _alignments[0].score
+        print(f"Score Percentage: {score_percent:.3f}")
+        return True, score
     elif verbose:
-        print(f"Score: {_alignments[0].score}")
-    return False, _alignments[0].score
+        print(f"Score Percentage: {score_percent:.3f}")
+    return False, score
