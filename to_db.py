307729d3cfe302b91b5ce8a907c21bbfc22f3406
Yannick Daniel Gibson <yannick.gibson@seznam.cz>
2024-11-02 17:06:15 +0100

    Export to db format

--- to_db.py
+++ to_db.py
@@ -4,28 +4,41 @@ from tqdm import tqdm
 import os
 import gzip
 import pandas as pd
+from natsort import natsorted
 
 SEQUENCING_DATA_PATH = "data/sequencing_data".replace('\\', "/")
-EXPORT_EVERY = 10
+EXPORT_EVERY = 1000
 EXPORT_PATH = "data/rows_for_db.csv"
+MAX_SEQUENCE_LENGTH = 10000
 
 
 def load_fastq_file(fastq_path: str, model: DNABERT2, rows: list) -> None:
     
     with gzip.open(fastq_path, 'rt') as file:
-        for i, record in tqdm(enumerate(SeqIO.parse(file, 'fastq')), total=4000):
+        pbar = tqdm(enumerate(SeqIO.parse(file, 'fastq')), total=4000)
+        for i, record in pbar:
             # Convert sequence to embedding
-            vector = model.get_embedding(str(record.seq)).detach().tolist()[0]
+            sequence_string = str(record.seq)
 
-            rows.append([fastq_path, i, vector])
-            if i % EXPORT_EVERY == 0:
+            # Truncate string if too long
+            sequence_string = sequence_string[:MAX_SEQUENCE_LENGTH]
+
+            # Update progress bar info before inference
+            pbar.set_description(f"Sequence length: {len(sequence_string)}, fastq_path: '{fastq_path}'")
+
+            # Inference: get embedding
+            vector = model.get_embedding(sequence_string).detach().tolist()[0]
+
+            rows.append([fastq_path, i, sequence_string, ",".join(vector)])
+            # show sequence_string len
+            if i == 0 or (i - 1) % EXPORT_EVERY == 0 or i == 3999:  # first or every x'th or last
                 print(f"Exporting {len(rows)} rows to '{EXPORT_PATH}'.")
-                df = pd.DataFrame(rows, columns=["file_path", "index", "vector"])
+                df = pd.DataFrame(rows, columns=["file_path", "index", "sequence_string", "sequence_vector"])
                 df.to_csv(EXPORT_PATH, index=False, sep=";")
 
 
 def load_barcode(folder_path: str, model: DNABERT2, rows: list) -> None:
-    fastq_files = sorted(os.listdir(folder_path))
+    fastq_files = natsorted(os.listdir(folder_path))
     fastq_files = [file for file in fastq_files if file.endswith(".fastq.gz")]
     print(f"Opening {len(fastq_files)} fastq files.")
     for fastq_file in fastq_files:
